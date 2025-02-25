99854966f637cdfa1e8c7756231bbce5edad0cbb
leemhoon00 <leemhoon000@gmail.com>
2025-02-25 23:19:54 +0900

    init

---
+++ src/db/types.ts
@@ -0,0 +1,139 @@
+import type { ColumnType } from "kysely";
+export type Generated<T> = T extends ColumnType<infer S, infer I, infer U>
+  ? ColumnType<S, I | undefined, U>
+  : ColumnType<T, T | undefined, T>;
+export type Timestamp = ColumnType<Date, Date | string, Date | string>;
+
+export type Feed = {
+    id: string;
+    authorId: string;
+    title: string;
+    cards: string[];
+    thumbnail: string;
+    isAI: Generated<boolean>;
+    createdAt: Generated<Timestamp>;
+    viewCount: Generated<number>;
+    likeCount: Generated<number>;
+    content: Generated<string>;
+};
+export type FeedComment = {
+    id: string;
+    feedId: string;
+    writerId: string;
+    parentId: string | null;
+    content: string;
+    mentionedUserId: string | null;
+    createdAt: Generated<Timestamp>;
+    likeCount: Generated<number>;
+};
+export type FeedCommentLike = {
+    feedCommentId: string;
+    userId: string;
+};
+export type Follow = {
+    followerId: string;
+    followingId: string;
+    createdAt: Generated<Timestamp>;
+};
+export type Like = {
+    userId: string;
+    feedId: string;
+    createdAt: Generated<Timestamp>;
+};
+export type Notification = {
+    id: string;
+    userId: string;
+    isRead: Generated<boolean>;
+    createdAt: Generated<Timestamp>;
+    data: unknown;
+};
+export type Post = {
+    id: string;
+    authorId: string;
+    title: string;
+    content: string;
+    type: number;
+    thumbnail: string | null;
+    createdAt: Generated<Timestamp>;
+    viewCount: Generated<number>;
+    commentCount: Generated<number>;
+};
+export type PostComment = {
+    id: string;
+    postId: string;
+    writerId: string | null;
+    parentId: string | null;
+    content: string;
+    mentionedUserId: string | null;
+    createdAt: Generated<Timestamp>;
+    likeCount: Generated<number>;
+    isDeleted: Generated<boolean>;
+};
+export type PostCommentLike = {
+    postCommentId: string;
+    userId: string;
+};
+export type PostLike = {
+    postId: string;
+    userId: string;
+};
+export type PostSave = {
+    postId: string;
+    userId: string;
+    createdAt: Generated<Timestamp>;
+};
+export type Report = {
+    id: string;
+    type: number;
+    userId: string;
+    refType: string;
+    refId: string;
+    content: string | null;
+    createdAt: Generated<Timestamp>;
+};
+export type Save = {
+    userId: string;
+    feedId: string;
+    createdAt: Generated<Timestamp>;
+};
+export type Tag = {
+    feedId: string;
+    tagName: string;
+};
+export type User = {
+    id: string;
+    provider: string;
+    providerId: string;
+    email: string;
+    name: string;
+    image: string | null;
+    description: Generated<string>;
+    backgroundImage: string | null;
+    links: Generated<string[]>;
+    followerCount: Generated<number>;
+    subscription: Generated<string[]>;
+    createdAt: Generated<Timestamp>;
+};
+export type View = {
+    userId: string;
+    feedId: string;
+    createdAt: Generated<Timestamp>;
+};
+export type DB = {
+    Feed: Feed;
+    FeedComment: FeedComment;
+    FeedCommentLike: FeedCommentLike;
+    Follow: Follow;
+    Like: Like;
+    Notification: Notification;
+    Post: Post;
+    PostComment: PostComment;
+    PostCommentLike: PostCommentLike;
+    PostLike: PostLike;
+    PostSave: PostSave;
+    Report: Report;
+    Save: Save;
+    Tag: Tag;
+    User: User;
+    View: View;
+};
---
+++ src/index.ts
@@ -0,0 +1,234 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
+import fs from 'fs';
+import xmlFormat from 'xml-formatter';
+import { db } from './db/database';
+
+const serviceUrl = 'https://www.grimity.com';
+const sitemapUrl = 'https://sitemap.grimity.com';
+const imageUrl = 'https://image.grimity.com';
+
+const rootFoler = `${__dirname}/../sitemap`;
+const feedFoler = `${__dirname}/../sitemap/feed`;
+const userFoler = `${__dirname}/../sitemap/user`;
+const postFoler = `${__dirname}/../sitemap/post`;
+
+if (!fs.existsSync(rootFoler)) {
+  fs.mkdirSync(rootFoler);
+}
+
+if (!fs.existsSync(feedFoler)) {
+  fs.mkdirSync(feedFoler);
+}
+
+if (!fs.existsSync(userFoler)) {
+  fs.mkdirSync(userFoler);
+}
+
+if (!fs.existsSync(postFoler)) {
+  fs.mkdirSync(postFoler);
+}
+
+async function main() {
+  const feedPage = await createFeedIndex();
+  const userPage = await createUserIndex();
+  const postPage = await createPostIndex();
+
+  const rootSiteMap = `
+    <?xml version="1.0" encoding="UTF-8"?>
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+      <url>
+        <loc>${serviceUrl}</loc>
+        <priority>1.0</priority>
+      </url>
+      <url>
+        <loc>${serviceUrl}/board</loc>
+      </url>
+    </urlset>
+  `;
+  fs.writeFileSync(
+    `${rootFoler}/root.xml`,
+    xmlFormat(rootSiteMap, {
+      indentation: '  ',
+      collapseContent: true,
+    })
+  );
+
+  const rootIndex = `
+    <?xml version="1.0" encoding="UTF-8"?>
+    <sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+      <sitemap>
+        <loc>${sitemapUrl}/root.xml</loc>
+      </sitemap>
+      ${Array.from({ length: feedPage }).map(
+        (_, i) =>
+          `
+        <sitemap>
+          <loc>${sitemapUrl}/feed/feed-${i + 1}.xml</loc>
+        </sitemap>
+      `
+      )}
+      ${Array.from({ length: userPage }).map(
+        (_, i) =>
+          `
+        <sitemap>
+          <loc>${sitemapUrl}/user/user-${i + 1}.xml</loc>
+        </sitemap>
+      `
+      )}
+      ${Array.from({ length: postPage }).map(
+        (_, i) =>
+          `
+        <sitemap>
+          <loc>${sitemapUrl}/post/post-${i + 1}.xml</loc>
+        </sitemap>
+      `
+      )}
+    </sitemapindex>
+  `;
+
+  fs.writeFileSync(
+    `${rootFoler}/index.xml`,
+    xmlFormat(rootIndex, {
+      indentation: '  ',
+      collapseContent: true,
+    })
+  );
+}
+
+main().then(() => {
+  db.destroy();
+});
+
+async function createFeedIndex() {
+  const feeds = await db
+    .selectFrom('Feed')
+    .select(['id', 'cards', 'createdAt'])
+    .execute();
+
+  let count = Math.floor(feeds.length / 50000) + 1;
+
+  for (let i = 0; i < count; i++) {
+    const sitemap = `
+      <?xml version="1.0" encoding="UTF-8"?>
+      <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+        xmlns:image="http://www.google.com/schemas/sitemap-image/1.1">
+        ${feeds
+          .slice(i * 50000, (i + 1) * 50000)
+          .map(
+            (feed) =>
+              `
+          <url>
+            <loc>${serviceUrl}/feed/${feed.id}</loc>
+            <lastmod>${feed.createdAt.toISOString().split('T')[0]}</lastmod>
+            ${feed.cards
+              .map(
+                (card) =>
+                  `
+              <image:image>
+                <image:loc>${imageUrl}/${card}</image:loc>
+              </image:image>
+            `
+              )
+              .join('')}
+          </url>
+        `
+          )
+          .join('')}
+      </urlset>
+    `;
+
+    fs.writeFileSync(
+      `${feedFoler}/feed-${i + 1}.xml`,
+      xmlFormat(sitemap, {
+        indentation: '  ',
+        collapseContent: true,
+      })
+    );
+  }
+
+  return count;
+}
+
+async function createUserIndex() {
+  const users = await db.selectFrom('User').select(['id']).execute();
+
+  let count = Math.floor(users.length / 50000) + 1;
+
+  for (let i = 0; i < count; i++) {
+    const sitemap = `
+      <?xml version="1.0" encoding="UTF-8"?>
+      <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+        ${users
+          .slice(i * 50000, (i + 1) * 50000)
+          .map(
+            (user) =>
+              `
+          <url>
+            <loc>${serviceUrl}/user/${user.id}</loc>
+          </url>
+        `
+          )
+          .join('')}
+      </urlset>
+    `;
+
+    fs.writeFileSync(
+      `${userFoler}/user-${i + 1}.xml`,
+      xmlFormat(sitemap, {
+        indentation: '  ',
+        collapseContent: true,
+      })
+    );
+  }
+
+  return count;
+}
+
+async function createPostIndex() {
+  const posts = await db
+    .selectFrom('Post')
+    .select(['id', 'createdAt', 'thumbnail'])
+    .execute();
+
+  let count = Math.floor(posts.length / 50000) + 1;
+
+  for (let i = 0; i < count; i++) {
+    const sitemap = `
+      <?xml version="1.0" encoding="UTF-8"?>
+      <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+        xmlns:image="http://www.google.com/schemas/sitemap-image/1.1">
+        ${posts
+          .slice(i * 50000, (i + 1) * 50000)
+          .map(
+            (post) =>
+              `
+          <url>
+            <loc>${serviceUrl}/post/${post.id}</loc>
+            <lastmod>${post.createdAt.toISOString().split('T')[0]}</lastmod>
+            ${
+              post.thumbnail
+                ? `
+              <image:image>
+                <image:loc>${imageUrl}/${post.thumbnail}</image:loc>
+              </image:image>
+            `
+                : ''
+            }
+          </url>
+        `
+          )
+          .join('')}
+      </urlset>
+    `;
+    fs.writeFileSync(
+      `${postFoler}/post-${i + 1}.xml`,
+      xmlFormat(sitemap, {
+        indentation: '  ',
+        collapseContent: true,
+      })
+    );
+  }
+
+  return count;
+}
